380910b0dcebf6863db1fdb93bcd057c455be5a8
Siri P R <siri.pr914@gmail.com>
2023-04-03 09:42:14 +0530

    wip: add visual components to detail view

---
+++ Scrumdinger/DetailView.swift
@@ -0,0 +1,24 @@
+//
+//  DetailView.swift
+//  Scrumdinger
+//
+//  Created by Siri P R on 01/04/23.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    let scrum: DailyScrum
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct DetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationView {
+            DetailView(scrum: DailyScrum.sampleData[0])
+        }
+    }
+}
